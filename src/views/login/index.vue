e6f41134151d8741873e743cf087c33f5de50578
lipengzhou <lpzmail@163.com>
2020-04-20 16:27:35 +0800

    用户登录-页面布局-使用Form表单组件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!--
+      el-form 表单组件
+      每个表单项都必须使用 el-form-item 组件包裹
+     -->
+    <el-form ref="form" :model="user" label-width="80px">
+      <el-form-item label="手机号">
+        <el-input v-model="user.mobile"></el-input>
+      </el-form-item>
+      <el-form-item label="验证码">
+        <el-input v-model="user.code"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -8,13 +24,22 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
