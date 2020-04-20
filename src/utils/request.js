f4a5e56cb35b97e6e1f1b9acfc6acaaf80d82f1f
lipengzhou <lpzmail@163.com>
2020-04-20 15:29:22 +0800

    项目初始化-封装请求模块

---
+++ src/utils/request.js
@@ -0,0 +1,29 @@
+/**
+ * 基于 axios 封装的请求模块
+ */
+import axios from 'axios'
+
+// axios()
+// axios.get()
+// axios.post()
+
+// 创建一个 axios 实例，说白了就是复制了一个 axios
+// 我们通过这个实例去发请求，把需要的配置配置给这个实例来处理
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
+})
+
+// 请求拦截器
+
+// 响应拦截器
+
+// 导出请求方法
+export default request
+
+// 谁要使用谁就加载 request 模块
+// import request from 'request.js'
+// request.xxx
+// request({
+//   method: ,
+//   url: ''
+// })
