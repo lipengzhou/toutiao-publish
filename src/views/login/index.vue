67eeb74e3a9a47f196b2ab603237a921464c4d74
lipengzhou <lpzmail@163.com>
2020-04-20 17:26:32 +0800

    用户登录-登陆中loading处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -29,6 +29,7 @@
           <el-button
             class="login-btn"
             type="primary"
+            :loading="loginLoading"
             @click="onLogin"
           >登录</el-button>
         </el-form-item>
@@ -50,7 +51,8 @@ export default {
         mobile: '', // 手机号
         code: '' // 验证码
       },
-      checked: false // 是否同意协议的选中状态
+      checked: false, // 是否同意协议的选中状态
+      loginLoading: false // 登录的 loading 状态
     }
   },
   computed: {},
@@ -65,6 +67,10 @@ export default {
       // 表单验证
 
       // 验证通过，提交登录
+
+      // 开启登陆中 loading...
+      this.loginLoading = true
+
       request({
         method: 'POST',
         url: '/mp/v1_0/authorizations',
@@ -78,9 +84,15 @@ export default {
           message: '登录成功',
           type: 'success'
         })
+
+        // 关闭 loading
+        this.loginLoading = false
       }).catch(err => { // 登录失败
         console.log('登录失败', err)
         this.$message.error('登录失败，手机号或验证码错误')
+
+        // 关闭 loading
+        this.loginLoading = false
       })
     }
   }
