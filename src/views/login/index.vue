2d5d1f04f8e1731e4b627eeb5e51a2d1c1338463
lipengzhou <lpzmail@163.com>
2020-04-20 16:56:45 +0800

    样式调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,31 +5,35 @@
       每个表单项都必须使用 el-form-item 组件包裹
      -->
 
-    <div class="login-head"></div>
-    <el-form class="login-form" ref="form" :model="user">
-      <el-form-item>
-        <el-input
-          v-model="user.mobile"
-          placeholder="请输入手机号"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input
-          v-model="user.code"
-          placeholder="请输入验证码"
-        ></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
-      </el-form-item>
-      <el-form-item>
-        <el-button
-          class="login-btn"
-          type="primary"
-          @click="onSubmit"
-        >登录</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="login-form-wrap">
+      <div class="login-head">
+        <div class="logo"></div>
+      </div>
+      <el-form class="login-form" ref="form" :model="user">
+        <el-form-item>
+          <el-input
+            v-model="user.mobile"
+            placeholder="请输入手机号"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="user.code"
+            placeholder="请输入验证码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            class="login-btn"
+            type="primary"
+            @click="onSubmit"
+          >登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -72,18 +76,24 @@ export default {
   align-items: center;
   background: url("./login_bg.jpg") no-repeat;
   background-size: cover;
-  .login-head {
-    width: 300px;
-    height: 57px;
-    background: url("./logo_index.png") no-repeat;
-    margin-bottom: 30px;
-  }
-  .login-form {
-    background-color: #fff;
-    padding: 50px;
+  .login-form-wrap {
     min-width: 300px;
-    .login-btn {
-      width: 100%;
+    padding: 30px 50px 10px;
+    background-color: #fff;
+    .login-head {
+      display: flex;
+      justify-content: center;
+      .logo {
+        width: 200px;
+        height: 57px;
+        background: url("./logo_index.png") no-repeat;
+        background-size: contain;
+      }
+    }
+    .login-form {
+      .login-btn {
+        width: 100%;
+      }
     }
   }
 }
