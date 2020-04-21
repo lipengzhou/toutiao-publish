a34b319901f8851abe1bd04f403985e0c4af0f23
lipengzhou <lpzmail@163.com>
2020-04-21 15:46:59 +0800

    首页-侧边导航-让导航栏和路由对应起来

--- src/views/layout/components/aside.vue
+++ src/views/layout/components/aside.vue
@@ -10,32 +10,33 @@
     background-color="#545c64"
     text-color="#fff"
     active-text-color="#ffd04b"
+    router
   >
-    <el-menu-item index="1">
+    <el-menu-item index="/">
       <i class="el-icon-setting"></i>
       <span slot="title">首页</span>
     </el-menu-item>
-    <el-menu-item index="2">
+    <el-menu-item index="/article">
       <i class="el-icon-menu"></i>
       <span slot="title">内容管理</span>
     </el-menu-item>
-    <el-menu-item index="3">
+    <el-menu-item index="/image">
       <i class="el-icon-document"></i>
       <span slot="title">素材管理</span>
     </el-menu-item>
-    <el-menu-item index="4">
+    <el-menu-item index="/publish">
       <i class="el-icon-setting"></i>
       <span slot="title">发布文章</span>
     </el-menu-item>
-    <el-menu-item index="5">
+    <el-menu-item index="/comment">
       <i class="el-icon-setting"></i>
       <span slot="title">评论管理</span>
     </el-menu-item>
-    <el-menu-item index="6">
+    <el-menu-item index="/fans">
       <i class="el-icon-setting"></i>
       <span slot="title">粉丝管理</span>
     </el-menu-item>
-    <el-menu-item index="7">
+    <el-menu-item index="/settings">
       <i class="el-icon-setting"></i>
       <span slot="title">个人设置</span>
     </el-menu-item>
