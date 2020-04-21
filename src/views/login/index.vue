ad345ab0156c5f63971fbe99bc4a4720f064bc2b
lipengzhou <lpzmail@163.com>
2020-04-21 10:24:14 +0800

    用户登录-表单验证-基本使用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,14 +9,26 @@
       <div class="login-head">
         <div class="logo"></div>
       </div>
-      <el-form class="login-form" ref="form" :model="user">
-        <el-form-item>
+      <!--
+        配置 Form 表单验证：
+        1、必须给 el-from 组件绑定 model 为表单数据对象
+        2、给需要验证的表单项 el-form-item 绑定 prop 属性
+           注意：prop 属性需要指定表单对象中的数据名称
+        3、通过 el-from 组件的 rules 属性配置验证规则
+       -->
+      <el-form
+        class="login-form"
+        ref="form"
+        :model="user"
+        :rules="formRules"
+      >
+        <el-form-item prop="mobile">
           <el-input
             v-model="user.mobile"
             placeholder="请输入手机号"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input
             v-model="user.code"
             placeholder="请输入验证码"
@@ -52,7 +64,18 @@ export default {
         code: '' // 验证码
       },
       checked: false, // 是否同意协议的选中状态
-      loginLoading: false // 登录的 loading 状态
+      loginLoading: false, // 登录的 loading 状态
+      formRules: { // 表单验证规则配置
+        // 要验证的数据名称：规则列表[]
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'change' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '请输入正确的号码格式', trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空', trigger: 'change' },
+          { pattern: /^\d{6}$/, message: '请输入正确的验证码格式' }
+        ]
+      }
     }
   },
   computed: {},
