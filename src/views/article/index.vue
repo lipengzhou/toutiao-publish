e0954865030f42877391a0bc8b97eff422896a06
lipengzhou <lpzmail@163.com>
2020-04-22 15:34:48 +0800

    内容管理-页面布局（面包屑和数据筛选）

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="article-container">内容管理</div>
+  <div class="article-container">
+    <!-- 面包屑路径导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- /面包屑路径导航 -->
+
+    <!-- 数据筛选表单 -->
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="状态">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="全部"></el-radio>
+          <el-radio label="草稿"></el-radio>
+          <el-radio label="待审核"></el-radio>
+          <el-radio label="审核通过"></el-radio>
+          <el-radio label="审核失败"></el-radio>
+          <el-radio label="已删除"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道">
+        <el-select v-model="form.region" placeholder="请选择频道">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="日期">
+        <el-date-picker
+          v-model="form.date1"
+          type="datetimerange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :default-time="['12:00:00']">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- /数据筛选表单 -->
+  </div>
 </template>
 
 <script>
@@ -8,13 +48,28 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
