f0d29b820ef8d065776accb1697733630e451aa7
lipengzhou <lpzmail@163.com>
2020-04-22 16:17:03 +0800

    内容管理-页面布局（样式调整）

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,76 +1,85 @@
 <template>
   <div class="article-container">
-    <!-- 面包屑路径导航 -->
-    <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>内容管理</el-breadcrumb-item>
-    </el-breadcrumb>
-    <!-- /面包屑路径导航 -->
+    <el-card class="filter-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- /面包屑路径导航 -->
+      </div>
+      <!-- 数据筛选表单 -->
+      <el-form ref="form" :model="form" label-width="40px" size="mini">
+        <el-form-item label="状态">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="全部"></el-radio>
+            <el-radio label="草稿"></el-radio>
+            <el-radio label="待审核"></el-radio>
+            <el-radio label="审核通过"></el-radio>
+            <el-radio label="审核失败"></el-radio>
+            <el-radio label="已删除"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="form.region" placeholder="请选择频道">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期">
+          <el-date-picker
+            v-model="form.date1"
+            type="datetimerange"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :default-time="['12:00:00']">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- /数据筛选表单 -->
+    </el-card>
 
-    <!-- 数据筛选表单 -->
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="状态">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="全部"></el-radio>
-          <el-radio label="草稿"></el-radio>
-          <el-radio label="待审核"></el-radio>
-          <el-radio label="审核通过"></el-radio>
-          <el-radio label="审核失败"></el-radio>
-          <el-radio label="已删除"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="频道">
-        <el-select v-model="form.region" placeholder="请选择频道">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="日期">
-        <el-date-picker
-          v-model="form.date1"
-          type="datetimerange"
-          start-placeholder="开始日期"
-          end-placeholder="结束日期"
-          :default-time="['12:00:00']">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
-    </el-form>
-    <!-- /数据筛选表单 -->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        根据筛选条件共查询到 46147 条结果：
+      </div>
+      <!-- 数据列表 -->
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%"
+        class="list-table"
+        size="mini"
+      >
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+      <!-- /数据列表 -->
 
-    <!-- 数据列表 -->
-    <el-table
-      :data="tableData"
-      stripe
-      border
-      style="width: 100%"
-    >
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table>
-    <!-- /数据列表 -->
-
-    <!-- 列表分页 -->
-    <el-pagination
-      layout="prev, pager, next"
-      background
-      :total="1000">
-    </el-pagination>
-    <!-- /列表分页 -->
+      <!-- 列表分页 -->
+      <el-pagination
+        layout="prev, pager, next"
+        background
+        :total="1000">
+      </el-pagination>
+      <!-- /列表分页 -->
+    </el-card>
   </div>
 </template>
 
@@ -122,4 +131,12 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.filter-card {
+  margin-bottom: 30px;
+}
+
+.list-table {
+  margin-bottom: 20px;
+}
+</style>
