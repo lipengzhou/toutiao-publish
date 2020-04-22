f0fff7aa648196ff24bfa016c1d57e2e41adcd4e
lipengzhou <lpzmail@163.com>
2020-04-22 15:50:06 +0800

    内容管理-页面布局（分页组件）

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -39,6 +39,38 @@
       </el-form-item>
     </el-form>
     <!-- /数据筛选表单 -->
+
+    <!-- 数据列表 -->
+    <el-table
+      :data="tableData"
+      stripe
+      border
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+    <!-- /数据列表 -->
+
+    <!-- 列表分页 -->
+    <el-pagination
+      layout="prev, pager, next"
+      background
+      :total="1000">
+    </el-pagination>
+    <!-- /列表分页 -->
   </div>
 </template>
 
@@ -58,7 +90,24 @@ export default {
         type: [],
         resource: '',
         desc: ''
-      }
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   },
   computed: {},
