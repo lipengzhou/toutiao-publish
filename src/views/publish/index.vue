a595bc8a7e952dcd30af33c6c5917c3343da3d61
lipengzhou <lpzmail@163.com>
2020-04-25 10:51:44 +0800

    发布文章-页面布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="publish-container">发布文章</div>
+  <div class="publish-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>发布文章</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- /面包屑路径导航 -->
+      </div>
+      <el-form ref="form" :model="form" label-width="40px">
+        <el-form-item label="标题">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="单图"></el-radio>
+            <el-radio label="三图"></el-radio>
+            <el-radio label="无图"></el-radio>
+            <el-radio label="自动"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="form.region" placeholder="请选择频道">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">发表</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,13 +45,28 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
