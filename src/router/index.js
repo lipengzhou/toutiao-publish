b61679e3f423e6f2568a8fcaa05a21505392bc6f
lipengzhou <lpzmail@163.com>
2020-04-25 10:40:48 +0800

    发布文章-创建页面组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Login from '@/views/login/'
 import Home from '@/views/home/'
 import Layout from '@/views/layout/'
 import Article from '@/views/article/'
+import Publish from '@/views/publish/'
 
 Vue.use(VueRouter)
 
@@ -43,6 +44,11 @@ const routes = [
         path: '/article',
         name: 'article',
         component: Article
+      },
+      {
+        path: '/publish',
+        name: 'publish',
+        component: Publish
       }
     ]
   }
---
+++ src/views/publish/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="publish-container">发布文章</div>
+</template>
+
+<script>
+export default {
+  name: 'PublishIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
