8df3c7ce419d1800127f8453964d92d5c3e7bfd9
lipengzhou <lpzmail@163.com>
2020-04-26 15:37:35 +0800

    素材管理-页面布局

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,5 +1,83 @@
 <template>
-  <div class="image-container">素材管理</div>
+  <div class="image-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>素材管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- /面包屑路径导航 -->
+        <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+      </div>
+      <div style="padding-bottom: 20px;">
+        <el-radio-group v-model="radio1" size="mini">
+          <el-radio-button label="全部"></el-radio-button>
+          <el-radio-button label="收藏"></el-radio-button>
+        </el-radio-group>
+      </div>
+      <!-- 素材列表 -->
+      <el-row :gutter="10">
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image
+            style="height: 100px"
+            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            fit="cover"
+          ></el-image>
+        </el-col>
+      </el-row>
+      <!-- /素材列表 -->
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,7 +86,9 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      radio1: '全部'
+    }
   },
   computed: {},
   watch: {},
