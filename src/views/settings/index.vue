4ab7a41e2704fa031e99691fc27bf1bcfee7fefc
lipengzhou <lpzmail@163.com>
2020-04-28 15:55:21 +0800

    个人设置-页面布局

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="settings-container">用户设置</div>
+  <div class="settings-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>个人设置</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- /面包屑路径导航 -->
+      </div>
+      <el-row>
+        <el-col :span="15">
+          <el-form ref="form" :model="form" label-width="70px">
+            <el-form-item label="编号">
+              123
+            </el-form-item>
+            <el-form-item label="手机">
+              456
+            </el-form-item>
+            <el-form-item label="媒体名称">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="媒体介绍">
+              <el-input type="textarea" v-model="form.desc"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+        <el-col :offset="2" :span="4">
+          <el-avatar
+            shape="square"
+            :size="150"
+            fit="cover"
+            src="http://toutiao.meiduo.site/Fv0sNupc0RlG2gnFoT-XXFpsNifI"
+          ></el-avatar>
+          <p>点击修改头像</p>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,13 +52,28 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
