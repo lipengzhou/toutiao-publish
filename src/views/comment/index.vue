c00c8b79143cc543949d482926d156e49718bfb3
lipengzhou <lpzmail@163.com>
2020-04-28 14:16:12 +0800

    评论管理-页面布局

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="comment-container">评论管理</div>
+  <div class="comment-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>评论管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- /面包屑路径导航 -->
+      </div>
+      <el-table
+        class="table-list"
+        :data="tableData"
+        style="width: 100%"
+        stripe
+      >
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="1"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+        background
+      >
+      </el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,14 +51,39 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    handleSizeChange () {},
+    handleCurrentChange () {}
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.table-list {
+  margin-bottom: 30px;
+}
+</style>
