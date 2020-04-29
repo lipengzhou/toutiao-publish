3f6c8e9c91253324446359492aee294208b5e1f6
lipengzhou <lpzmail@163.com>
2020-04-29 16:19:02 +0800

    echarts使用示例

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="fans-container">粉丝管理</div>
+  <div class="fans-container">
+    <!-- 1. 为 ECharts 准备一个具备大小（宽高）的画布容器 DOM -->
+    <div ref="main" style="width: 600px;height:400px;"></div>
+
+    <h2>地图图表示例</h2>
+    <map-demo />
+  </div>
 </template>
 
 <script>
+// 2. 加载 echarts
+import echarts from 'echarts'
+import MapDemo from './components/map-demo'
+
 export default {
   name: 'FansIndex',
-  components: {},
+  components: {
+    MapDemo
+  },
   props: {},
   data () {
     return {}
@@ -13,7 +25,36 @@ export default {
   computed: {},
   watch: {},
   created () {},
-  mounted () {},
+  // 注意：初始化操作 DOM 一定要写到 mounted 钩子函数中
+  mounted () {
+    // 3. 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(this.$refs.main)
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫123', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    }
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
+  },
   methods: {}
 }
 </script>
