632e4ece17201c9eed656447fcdf86c4deff1ee7
lipengzhou <lpzmail@163.com>
2020-04-30 11:35:42 +0800

    文章封面处理-上传图片

--- src/views/publish/components/upload-cover.vue
+++ src/views/publish/components/upload-cover.vue
@@ -1,21 +1,111 @@
 <template>
-  <div class="upload-cover">文章封面</div>
+  <div class="upload-cover" @click="showCoverSelect">
+    <div class="cover-wrap">
+      <img
+        class="cover-image"
+        ref="cover-image"
+      >
+    </div>
+
+    <el-dialog
+      title="选择封面"
+      :visible.sync="dialogVisible"
+      append-to-body
+    >
+      <el-tabs v-model="activeName" type="card">
+        <el-tab-pane label="素材库" name="first">
+          素材库的内容
+        </el-tab-pane>
+        <el-tab-pane label="上传图片" name="second">
+          <input
+            type="file"
+            ref="file"
+            @change="onFileChange"
+          >
+          <img
+            width="100"
+            ref="preview-image"
+          >
+        </el-tab-pane>
+      </el-tabs>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="onCoverConfirm"
+        >确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { uploadImage } from '@/api/image'
+
 export default {
   name: 'UploadCover',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      dialogVisible: false,
+      activeName: 'second'
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    showCoverSelect () {
+      // 展示选择封面的弹窗
+      this.dialogVisible = true
+    },
+
+    onFileChange () {
+      // 获取文件对象
+      const file = this.$refs.file.files[0]
+
+      // 图片预览
+      const blob = window.URL.createObjectURL(file)
+      this.$refs['preview-image'].src = blob
+
+      // 防止用户选择同一个文件不触发 change 事件
+      // this.$refs.file.value = ''
+    },
+
+    onCoverConfirm () {
+      // 如果 tab 是上传图片，并且 input-file 有选择的文件，才执行上传图片的操作
+      if (this.activeName === 'second') {
+        const file = this.$refs.file.files[0]
+        if (!file) {
+          this.$message('请选择文件')
+          return
+        }
+        // 执行上传文件的操作
+        const fd = new FormData()
+        fd.append('image', file)
+        uploadImage(fd).then(res => {
+          console.log(res)
+          // 关闭弹出层
+          this.dialogVisible = false
+          // 展示上传的图片
+          this.$refs['cover-image'].src = res.data.data.url
+        })
+      }
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.cover-wrap {
+  width: 150px;
+  height: 120px;
+  border: 1px solid #000;
+  .cover-image {
+    height: 120px;
+    max-width: 100%;
+  }
+}
+</style>
