284be350a183ea6440e989fa9618a735cda1c76e
lipengzhou <lpzmail@163.com>
2020-04-30 14:45:11 +0800

    文章封面处理-素材库-抽取组件

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -10,251 +10,29 @@
         <!-- /面包屑路径导航 -->
         <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
       </div>
-      <div class="action-head">
-        <el-radio-group
-          v-model="collect"
-          size="mini"
-          @change="loadImages(1)"
-        >
-          <el-radio-button
-            :label="false"
-          >全部</el-radio-button>
-          <el-radio-button
-            :label="true"
-          >收藏</el-radio-button>
-        </el-radio-group>
-        <el-button
-          size="mini"
-          type="success"
-          @click="dialogUploadVisible = true"
-        >上传素材</el-button>
-      </div>
-      <!-- 素材列表 -->
-      <el-row :gutter="10">
-        <el-col
-          :xs="12"
-          :sm="6"
-          :md="6"
-          :lg="4"
-          v-for="(img, index) in images"
-          :key="index"
-          class="image-item"
-        >
-          <el-image
-            style="height: 100px"
-            :src="img.url"
-            fit="cover"
-          ></el-image>
-          <div class="image-action">
-            <!--
-              class 样式绑定
-               {
-                  CSS类名: 布尔值
-               }
-               true：作用类名
-               false：不作用类名
-             -->
-            <el-button
-              type="warning"
-              :icon="img.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'"
-              circle
-              size="small"
-              @click="onCollect(img)"
-              :loading="img.loading"
-            ></el-button>
-            <el-button
-              size="small"
-              type="danger"
-              icon="el-icon-delete-solid"
-              circle
-              :loading="img.loading"
-              @click="onDelete(img)"
-            ></el-button>
-            <!-- <i
-              :class="{
-                'el-icon-star-on': img.is_collected,
-                'el-icon-star-off': !img.is_collected
-              }"
-              @click="onCollect(img)"
-            ></i> -->
-            <!-- <i class="el-icon-delete-solid"></i> -->
-            <!-- <el-button size="mini" icon="el-icon-delete-solid" circle></el-button> -->
-          </div>
-        </el-col>
-      </el-row>
-      <!-- /素材列表 -->
-
-      <!-- 数据分页 -->
-      <!--
-        分页数据改变以后，页码不会变化
-        它需要单独处理高亮的页码
-       -->
-      <el-pagination
-        background
-        layout="prev, pager, next"
-        :total="totalCount"
-        :page-size="pageSize"
-        :current-page.sync="page"
-        @current-change="onPageChange"
-      >
-      </el-pagination>
-      <!-- /数据分页 -->
+      <!-- 图片列表 -->
+      <image-list />
+      <!-- /图片列表 -->
     </el-card>
-
-    <el-dialog
-      title="上传素材"
-      :visible.sync="dialogUploadVisible"
-      :append-to-body="true"
-    >
-      <!--
-        upload 组件本身就支持请求提交上传操作，说白了你使用它不用自己去发请求，它会自己发。
-        请求方法：默认就是 POST
-        请求路径：action，必须写完整路径
-        请求头：headers
-       -->
-      <el-upload
-        class="upload-demo"
-        drag
-        action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
-        :headers="uploadHeaders"
-        name="image"
-        multiple
-        :show-file-list="false"
-        :on-success="onUploadSuccess"
-      >
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-      </el-upload>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import {
-  getImages,
-  collectImage,
-  deleteImage
-} from '@/api/image'
+import ImageList from './components/image-list'
 
 export default {
   name: 'ImageIndex',
-  components: {},
-  props: {},
-  data () {
-    const user = JSON.parse(window.localStorage.getItem('user'))
-    return {
-      collect: false, // 默认查询全部素材列表
-      images: [], // 图片素材列表
-      dialogUploadVisible: false,
-      uploadHeaders: {
-        Authorization: `Bearer ${user.token}`
-      },
-      totalCount: 0, // 总数据条数
-      pageSize: 20, // 每页大小
-      page: 1 // 当前页码
-    }
+  components: {
+    ImageList
   },
+  props: {},
+  data () {},
   computed: {},
   watch: {},
-  created () {
-    // 页面初始化加载第 1 页数据
-    this.loadImages(1)
-  },
+  created () {},
   mounted () {},
-  methods: {
-    loadImages (page = 1) {
-      // 重置高亮页码，防止数据和页码不对应
-      this.page = page
-      getImages({
-        collect: this.collect,
-        page,
-        per_page: this.pageSize
-      }).then(res => {
-        const results = res.data.data.results
-        results.forEach(img => {
-          // img 对象本来没有 loading 数据
-          // 我们这里收到的往里面添加该数据是用来控制每个收藏按钮的 loading 状态
-          img.loading = false
-        })
-        this.images = results
-        this.totalCount = res.data.data.total_count
-      })
-    },
-
-    // onCollectChange () {
-    //   this.loadImages(1)
-    // },
-
-    onUploadSuccess () {
-      // 关闭对话框
-      this.dialogUploadVisible = false
-
-      // 更新素材列表
-      this.loadImages(this.page)
-
-      this.$message({
-        type: 'success',
-        message: '上传成功'
-      })
-    },
-
-    onPageChange (page) {
-      this.loadImages(page)
-    },
-
-    onCollect (img) {
-      // 展示 loading
-      img.loading = true
-      collectImage(img.id, !img.is_collected).then(res => {
-        // 更新视图状态
-        img.is_collected = !img.is_collected
-        // 关闭 loading
-        img.loading = false
-      })
-      // if (img.is_collected) {
-      //   // 已收藏，取消收藏
-      //   collectImage(img.id, false)
-      // } else {
-      //   // 没有收藏，添加收藏
-      //   collectImage(img.id, true)
-      // }
-    },
-
-    onDelete (img) {
-      img.loading = true
-      deleteImage(img.id).then(res => {
-        // 重新加载数据列表
-        this.loadImages(this.page)
-        img.loading = false
-      })
-    }
-  }
+  methods: {}
 }
 </script>
 
-<style scoped lang="less">
-.action-head {
-  padding-bottom: 20px;
-  display: flex;
-  justify-content: space-between;
-}
-
-.image-item {
-  position: relative;
-}
-
-.image-action {
-  font-size: 25px;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  color: #fff;
-  height: 40px;
-  background-color: rgba(204, 204, 204, .5);
-  position: absolute;
-  bottom: 4px;
-  left: 5px;
-  right: 5px;
-}
-</style>
+<style scoped lang="less"></style>
