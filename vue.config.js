e59ae7939ccba72779f8bb937d1e649b6773faca
lipengzhou <lpzmail@163.com>
2021-04-20 10:27:43 +0800

    fix: change api url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://ttapi.research.itcast.cn/',
+        target: 'http://api-toutiao-web.itheima.net',
+        // target: 'http://ttapi.research.itcast.cn/',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
